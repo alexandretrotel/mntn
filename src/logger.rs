a40ea1fd3d7a6e11a623b65536a7470c07538c6f
Alexandre Trotel <trotelalexandre@proton.me>
2025-09-28 11:31:48 +0200

    refactor: update logging and path handling to use the .mntn directory for backups

--- src/logger.rs
+++ src/logger.rs
@@ -2,15 +2,14 @@ use chrono::Local;
 use std::fs::OpenOptions;
 use std::io::Write;
 
-use crate::utils::paths::get_base_dirs;
+use crate::utils::paths::get_mntn_dir;
 
-/// Appends a timestamped log message to a file named `mntn.log` in the user's home directory.
+/// Appends a timestamped log message to a file named `mntn.log` in the `.mntn` directory.
 ///
 /// The log entry format is: `[YYYY-MM-DD HH:MM:SS] message`
 pub fn log(message: &str) {
-    let base_dirs = get_base_dirs();
-    let home_dir = base_dirs.home_dir();
-    let log_path = home_dir.join("mntn.log"); // ~/mntn.log
+    let mntn_dir = get_mntn_dir();
+    let log_path = mntn_dir.join("mntn.log"); // ~/.mntn/mntn.log
     let timestamp = Local::now().format("[%Y-%m-%d %H:%M:%S]").to_string();
 
     let mut file = OpenOptions::new()
--- src/utils/paths.rs
+++ src/utils/paths.rs
@@ -2,24 +2,28 @@ use directories_next::BaseDirs;
 use std::path::PathBuf;
 
 /// Relative path to the directory used for storing general backup files.
-pub const BACKUP_DIR: &str = "dotfiles/backups";
+pub const BACKUP_DIR: &str = ".mntn/backups";
 
 /// This directory is a subdirectory of `BACKUP_DIR` and is used when a file or folder
 /// would be replaced by a symlink, allowing safe restoration if needed.
-pub const SYMLINK_BACKUP_DIR: &str = "dotfiles/backups/symlinks";
+pub const SYMLINK_BACKUP_DIR: &str = ".mntn/backups/symlinks";
+
+pub fn get_mntn_dir() -> PathBuf {
+    let base_dirs = get_base_dirs();
+    let home_dir = base_dirs.home_dir();
+    home_dir.join(".mntn")
+}
 
 /// Resolves the full path to the general backup directory (`BACKUP_DIR`) inside the user's home.
 pub fn get_backup_path() -> PathBuf {
-    let base_dirs = get_base_dirs();
-    let home_dir = base_dirs.home_dir();
-    home_dir.join(BACKUP_DIR)
+    let mntn_dir = get_mntn_dir();
+    mntn_dir.join(BACKUP_DIR)
 }
 
 /// Resolves the full path to the symlink-specific backup directory (`SYMLINK_BACKUP_DIR`) inside the user's home.
 pub fn get_symlink_backup_path() -> PathBuf {
-    let base_dirs = get_base_dirs();
-    let home_dir = base_dirs.home_dir();
-    home_dir.join(SYMLINK_BACKUP_DIR)
+    let mntn_dir = get_mntn_dir();
+    mntn_dir.join(SYMLINK_BACKUP_DIR)
 }
 
 pub fn get_base_dirs() -> BaseDirs {
