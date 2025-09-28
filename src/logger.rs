7378f243e556f2ca479dc139f68ac61cc6751762
Alexandre Trotel <trotelalexandre@proton.me>
2025-09-28 08:58:47 +0200

    refactor: remove panic documentation and examples from log function

--- src/logger.rs
+++ src/logger.rs
@@ -5,19 +5,6 @@ use std::io::Write;
 /// Appends a timestamped log message to a file named `mntn.log` in the user's home directory.
 ///
 /// The log entry format is: `[YYYY-MM-DD HH:MM:SS] message`
-///
-/// # Panics
-///
-/// This function will panic if:
-/// - The user's home directory cannot be determined.
-/// - The log file cannot be created or opened for appending.
-/// - Writing to the log file fails.
-///
-/// # Examples
-///
-/// ```
-/// log("Application started");
-/// ```
 pub fn log(message: &str) {
     let log_path = dirs_next::home_dir().unwrap().join("mntn.log"); // ~/mntn.log
     let timestamp = Local::now().format("[%Y-%m-%d %H:%M:%S]").to_string();
